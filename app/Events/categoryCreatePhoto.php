ae88e3c208c6726a273898599f15eb231a908ffa
niloofar.noorbakhsh <niloofar.noorbakhsh@yahoo.com>
2023-04-09 17:36:03 +0430

    two events is created for create/delete image of the category

---
+++ app/Events/categoryCreatePhoto.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Category;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class categoryCreatePhoto
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(public $images,public Category $category)
+    {
+
+    }
+
+    public function getImages()
+    {
+    return $this->images;
+    }
+
+    public function getCategory()
+    {
+        return $this->category;
+    }
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
